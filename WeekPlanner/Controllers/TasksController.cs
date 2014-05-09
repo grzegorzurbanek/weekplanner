efdd53a9da5d1adc90a15dc30faa2235ab6e187f
grzegorzurbanek <grzegorz.urbanek@gmail.com>
2014-05-10 00:17:23 +0200

    First commit
    New project set up, bootstrap added

---
+++ WeekPlanner/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeekPlanner.Models;
+
+namespace WeekPlanner.Controllers
+{
+    public class TasksController : Controller
+    {
+        //
+        // GET: /Tasks/
+
+        public ActionResult Index()
+        {
+            var model = new List<Task>();
+            model.Add(new Task() { Id = 1, Title = "Zadanie 1" });
+            model.Add(new Task() { Id = 2, Title = "Zadanie 2" });
+            return View(model);
+        }
+
+    }
+}
